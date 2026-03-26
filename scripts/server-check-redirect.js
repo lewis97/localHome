ba42086403e02db549eb02b1b96b10328d3f19ee
Lewis smith <lewissmith@Lewiss-Air.mynet>
2026-03-26 15:39:14 +0000

    scripts refactor

---
+++ scripts/server-check-redirect.js
@@ -0,0 +1,12 @@
+// Redirect to the flame server if it is running:
+async function checkServer() {
+  try {
+    const res = await fetch("http://home.home", { mode: "no-cors" });
+    window.location.href = "http://home.home";
+  } catch (e) {
+    console.log("Server not reachable, staying local.");
+  }
+}
+
+// Comment out for testing - force using local version
+// checkServer()
